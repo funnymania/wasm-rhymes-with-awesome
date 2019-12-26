5e6ced18cdf71d3d2c7d4f7ebfa74f0aef5bdf09
sophie <2amslekshun@gmail.com>
2019-12-26 14:17:42 -0800

    Add a demo todo

--- client/miner.js
+++ client/miner.js
@@ -1,11 +1,17 @@
 async function mineAway() {
-  const result = {} 
-  // result.data = await fetchWASM()
-  result.title = 'WASM cryptomining Speed'
-  addResultToDisplay(result)
+  const WASMResult = {} 
+  WASMResult.title = 'WASM cryptomining Speed'
+  // WASMResult.data = await fetchWASM()
+
+  const JSResult = {}
+  JSResult.title = 'JS cryptomining Speed'
+  // JSResult.data = await benchmarkJS()
+
+  addResultToDisplay(WASMResult)
+  addResultToDisplay(JSResult)
 }
 
-// TODO: Complete this method
+// DEMO: Complete this method
 async function fetchWASM() {
   console.log('Fetching WASM...')
   await WebAssembly.instantiateStreaming(fetch('simple.wasm'), importObject)
@@ -14,11 +20,22 @@ async function fetchWASM() {
     }); 
 }
 
+// TODO: Complete this method
+async function benchmarkJS() {
+  // Start a counter for number of SHA256 run. 
+  let counter = 0
+
+  // Compute SHA256 over and over again for 30 seconds
+
+  // Return counter
+  return counter
+}
+
 function addResultToDisplay(result) {
   console.log('Display results...')
 
   // Create results element
-  let newRes = document.createElement('span')
+  let newRes = document.createElement('div')
   newRes.classList.add('results-row')
 
   let newResText = document.createTextNode(`${result.title}: ${result.data}`)
