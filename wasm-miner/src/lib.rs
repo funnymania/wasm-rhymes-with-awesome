295f6c12b0f13287556420cd652755f3e9fc278c
funnymania <2amslekshun@gmail.com>
2019-12-31 05:00:07 -0800

    Stub out miner fun

--- wasm-miner/src/lib.rs
+++ wasm-miner/src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::i64;
+use sha2::{Sha256, digest}
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -16,12 +18,31 @@ extern {
 #[wasm_bindgen]
 pub fn greet(audience: &str) {
     alert(&format!("Hello, {}", audience));
-}
+}:wasm_bindgen
 
-// TODO: SHA256 here
 #[wasm_bindgen]
 pub fn roll_many_sided_die() {
-//     let mut counter = 0;
+    // Roll a large amount of dice
+    alert("WASM, rolling!");
 
-//     counter
+    // Nonce/Counter of mining runs.
+    let mut nonce = 0;
+
+    let headerData = i64::from_str_radix("81cd02ab7e569", 16)
+
+    while nonce < 100000 {
+        let mut header_and_nonce = headerData + nonce;
+
+        let hasher = Sha256::new();
+        hasher.input(header_and_nonce);
+
+        let digest1 = hasher.result();
+        hasher.input(digest1);
+
+        let digest2 = hasher.result()
+        
+        nonce += 1;
+    }
+
+    return
 }
